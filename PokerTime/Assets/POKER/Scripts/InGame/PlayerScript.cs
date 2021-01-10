f4335bf6213bb65ab603195925308f5636a95b80
Gaurang318 <gaurangpitroda@rocketmail.com>
2021-01-10 11:55:38 +0530

    10_01_21_11_55AM
    Club in game highlight winning cards

--- PokerTime/Assets/POKER/Scripts/InGame/PlayerScript.cs
+++ PokerTime/Assets/POKER/Scripts/InGame/PlayerScript.cs
@@ -725,15 +725,15 @@ public class PlayerScript : MonoBehaviour
                             if (i == 0)
                             {
                                 cardsImage[i].transform.localPosition = new Vector3(-40, -5);
-                                cardsImage[i].transform.localScale = new Vector3(0.55f, 0.55f);
-                                //cardsImage[i].transform.localScale = new Vector3(2f, 2f);
+                                //cardsImage[i].transform.localScale = new Vector3(0.55f, 0.55f);
+                                cardsImage[i].transform.localScale = new Vector3(2f, 2f);
                                 cardsImage[i].transform.localRotation = Quaternion.Euler(0, 0, 0);
                             }
                             if (i == 1)
                             {
                                 cardsImage[i].transform.localPosition = new Vector3(-25, -5);
-                                cardsImage[i].transform.localScale = new Vector3(0.55f, 0.55f);
-                                //cardsImage[i].transform.localScale = new Vector3(2f, 2f);
+                                //cardsImage[i].transform.localScale = new Vector3(0.55f, 0.55f);
+                                cardsImage[i].transform.localScale = new Vector3(2f, 2f);
                                 cardsImage[i].transform.localRotation = Quaternion.Euler(0, 0, 0);
                             }
 
@@ -773,33 +773,33 @@ public class PlayerScript : MonoBehaviour
                 {
                     if (!isItMe)
                     {
-                        if (i == 0)
-                        {
-                            cardsImage[i].transform.localPosition = new Vector3(0, 0);
-                            cardsImage[i].transform.localScale = new Vector3(0.35f, 0.35f);
-                        }
-                        if (i == 1)
-                        {
-                            cardsImage[i].transform.localPosition = new Vector3(16, 0);
-                            cardsImage[i].transform.localScale = new Vector3(0.35f, 0.35f);
-
-                            if (cardsImage[i].transform.localRotation.z == 0)
-                                cardsImage[i].transform.localRotation = Quaternion.Euler(0, 0, -18.12f);
-                        }
-
                         //if (i == 0)
                         //{
                         //    cardsImage[i].transform.localPosition = new Vector3(0, 0);
-                        //    cardsImage[i].transform.localScale = new Vector3(1f, 1f);
+                        //    cardsImage[i].transform.localScale = new Vector3(0.35f, 0.35f);
                         //}
                         //if (i == 1)
                         //{
-                        //    cardsImage[i].transform.localPosition = new Vector3(5, 0);
-                        //    cardsImage[i].transform.localScale = new Vector3(1f, 1f);
+                        //    cardsImage[i].transform.localPosition = new Vector3(16, 0);
+                        //    cardsImage[i].transform.localScale = new Vector3(0.35f, 0.35f);
 
                         //    if (cardsImage[i].transform.localRotation.z == 0)
-                        //        cardsImage[i].transform.localRotation = Quaternion.Euler(0, 0, -4f);
+                        //        cardsImage[i].transform.localRotation = Quaternion.Euler(0, 0, -18.12f);
                         //}
+
+                        if (i == 0)
+                        {
+                            cardsImage[i].transform.localPosition = new Vector3(0, 0);
+                            cardsImage[i].transform.localScale = new Vector3(1f, 1f);
+                        }
+                        if (i == 1)
+                        {
+                            cardsImage[i].transform.localPosition = new Vector3(5, 0);
+                            cardsImage[i].transform.localScale = new Vector3(1f, 1f);
+
+                            if (cardsImage[i].transform.localRotation.z == 0)
+                                cardsImage[i].transform.localRotation = Quaternion.Euler(0, 0, -4f);
+                        }
                     }
                     cardsImage[i].sprite = CardsManager.instance.GetCardBackSideSprite();
                 }
